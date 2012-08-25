1a96529c7093ec6c21e42ff8f9f58aa9e7d6c5b9
Castorflex <merle.antoine@gmail.com>
2012-08-25 04:40:58 +0200

    added message pojo

---
+++ src/fr/castorflex/android/quickanswer/pojos/Message.java
@@ -0,0 +1,35 @@
+package fr.castorflex.android.quickanswer.pojos;
+
+import android.telephony.SmsMessage;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Castorflex
+ * Date: 25/08/12
+ * Time: 04:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class Message {
+
+    private String sender;
+    private String message;
+
+    public Message(String sender, SmsMessage... messages)
+    {
+        this.sender = sender;
+        message = "";
+
+        for(int i = 0 ; i < messages.length ; ++i)
+        {
+            message += messages[i];
+        }
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
